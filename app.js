1dcff989dca164143ea891b60864a05dfdc9d4ad
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-04 23:46:05 -0300

    Inclusão de texto

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 let openShopping = document.querySelector('.shopping');
 let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
