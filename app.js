a61f6fc4b40a711fd8b33c0bd2dc675e0fc172a0
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2025-01-06 23:05:17 -0300

    Inclusão de texto

--- app.js
+++ app.js
@@ -52,15 +52,14 @@ let products = [
 ];
 let listCards = [];
 function initApp(){
-  products.forEach((value,key)=>{
+  products.forEach((value, key)=>{
     let newDiv = document.createElement('div');
     newDiv.classList.add('item');
     newDiv.innerHTML = `
     <img src="image/${value.image}"/>
     <div class="title">${value.name}</div>
     <div class="price">${value.price.toLocaleString()}</div>
-    <button onclick="addToCard(${key})">Add To Card</button>
-    `;
+    <button onclick="addToCard(${key})">Add To Card</button>`;
     list.appendChild(newDiv);
   })
 }
@@ -85,11 +84,26 @@ function reloadCard(){
       newDiv.innerHTML = ` 
         <div><img src="image/${value.image}"/></div>
         <div>${value.name}</div>
+        <div>${value.price.toLocaleString()}</div>
         <div>${value.quantity}</div>
+        <div>
+          <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+          <div class="count">${value.quantity}</div>
+          <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+          </div>
       `;
       listCard.appendChild(newDiv);
     }
   })
   total.innerText = totalPrice.toLocaleString();
   quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+  if(quantity == 0){
+    delete listCards[key];
+  }else{
+    listCards[key].quantity = quantity;
+    listCards[key].price = quantity * products[key].price;
+  }
+  reloadCard();
 }
